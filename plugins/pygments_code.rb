ca96fd8b9a4fa72b684971764d52a1c40c56d8d9
姚嘉俊 <jeromeyjj@gmail.com>
2012-04-23 18:16:41 +0800

    add weibo and disqus

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -5,6 +5,7 @@ require 'digest/md5'
 PYGMENTS_CACHE_DIR = File.expand_path('../../.pygments-cache', __FILE__)
 FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
 
+
 module HighlightCode
   def highlight(str, lang)
     lang = 'ruby' if lang == 'ru'
